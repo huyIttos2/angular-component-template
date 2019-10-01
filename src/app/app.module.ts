ce464b6e136265ec5ea785ff9f28928bc916ed62
huyItto <huyittos2071195@gmail.com>
2019-10-01 11:04:54 +0700

    update register-final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameCardComponent } from './name-card/name-card.component';
 import { ProgessBarComponent } from './progess-bar/progess-bar.component';
 import { TodoComponent } from './todo/todo.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterFinalComponent } from './register-final/register-final.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameCardComponent,
     ProgessBarComponent,
-    TodoComponent
+    TodoComponent,
+    RegisterComponent,
+    RegisterFinalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
